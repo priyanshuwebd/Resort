4001f1e1ccdf284e489fdcd7edfed362e3ed567d
priyanshupande <priyanshupande23@gmail.com>
2024-12-26 22:36:48 +0530

    services page and room page complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import Hero from '../Components/Hero'
-import Banner from '../Components/Banner'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import Hero from '../Components/Hero';
+import Banner from '../Components/Banner';
+import { Link } from 'react-router-dom';
+import Services from '../Components/Services';
 
-export default function Home(){
-  return <Hero>
-    <Banner title="Luxurious Rooms" subtitle="deluxe rooms starting at $299">
-      <Link to='/rooms' className='btn-primary'>
-        our rooms
-      </Link> 
-    </Banner>
-  </Hero>
+export default function Home() {
+  return (
+    <>
+      <Hero>
+        <Banner title="Luxurious Rooms" subtitle="Deluxe rooms starting at $299">
+          <Link to="/rooms" className="btn-primary">
+            Our Rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </>
+  );
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import Hero from '../Components/Hero'
+import Banner from '../Components/Banner'
+import { Link } from 'react-router-dom'
 
 const Rooms = () => {
-  return <Hero hero="roomsHero" /> 
+  return <Hero hero="roomsHero">
+
+    <Banner title="our rooms">
+    <Link to="/"  className='btn-primary'>
+      return home
+      </Link>
+    </Banner>
+    
+  </Hero>
   
 }
 
